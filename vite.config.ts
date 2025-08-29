e5729dc42c8cb406f38d1841f702d33956ae5061
Nemskri <kalpesh.maru@code-b.dev>
2025-08-30 01:23:52 +0530

    deploy mentsetup

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-
-
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/", // Changed to match your repo name
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
